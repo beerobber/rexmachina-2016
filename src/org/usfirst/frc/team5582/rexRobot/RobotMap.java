dac5db00acdea362060f264860b3b73bee72f582
Chris Taylor <beerobber@gmail.com>
2016-01-18 22:42:17 -0500

    First commit

---
+++ src/org/usfirst/frc/team5582/rexRobot/RobotMap.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team5582.rexRobot;
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+    // For example to map the left and right motors, you could define the
+    // following variables to use with your drivetrain subsystem.
+    public static int leftMotorCAN = 4;
+    public static int rightMotorCAN = 3;
+    
+    // If you are using multiple modules, make sure to define both the port
+    // number and the module. For example you with a rangefinder:
+    // public static int rangefinderPort = 1;
+    // public static int rangefinderModule = 1;
+}
