de4132bb64ce043219d16cdedae2d849c628ed4b
Erik Heitshusen <eheitshusen@yahoo.com>
2016-02-20 07:38:51 -0500

    preparing to pull

--- src/org/usfirst/frc/team5582/rexRobot/OI.java
+++ src/org/usfirst/frc/team5582/rexRobot/OI.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team5582.rexRobot;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.Joystick;
-// import org.usfirst.frc.team5582.rexRobot.commands.ErikFollowObject;
+import org.usfirst.frc.team5582.rexRobot.commands.ErikFollowObject;
 import edu.wpi.first.wpilibj.DriverStation;
 
 
@@ -31,7 +31,6 @@ public class OI {
     // until it is finished as determined by it's isFinished method.
     // button.whenPressed(new ExampleCommand());
     
-	// WHEEL ARMS STATE
     public enum WheelArmState {
     		DOWN("down"), UP("up"), STOP("stop");
     		String state;
@@ -44,7 +43,7 @@ public class OI {
     			return "WheelArmState" + this.state;
     		}
     }
-    // WINCH STATE
+    
     public enum WinchState {
 		DOWN("down"), UP("up"), STOP("stop");
 		String state;
@@ -54,23 +53,10 @@ public class OI {
 		}
 		
 		public String toString() {
-			return "Winch State" + this.state;
+			return "WinchState" + this.state;
 		}
 }
-    // BALL ARMS STATE
-    public enum BallArmsState {
-		DOWN("down"), UP("up"), STOP("stop");
-		String state;
-		
-		private BallArmsState(String stateIn) {
-			this.state = stateIn;
-		}
-		
-		public String toString() {
-			return "Ball Arm State" + this.state;
-		}
-}
-    // BOTTOM LIFT STATE
+    
     public enum BottomLiftState {
 		DOWN("down"), UP("up"), STOP("stop");
 		String state;
@@ -83,7 +69,7 @@ public class OI {
 			return "BottomLiftState" + this.state;
 		}
     }
-    // TOP LIFT STATE
+    
     public enum TopLiftState {
 		DOWN("down"), UP("up"), STOP("stop");
 		String state;
@@ -96,7 +82,7 @@ public class OI {
 			return "TopLiftState" + this.state;
 		}
     }
-    // BALL SPINNER STATE
+    
     public enum BallSpinnerState {
  		OUT("out"), IN("in"), STOP("stop");
  		String state;
@@ -135,9 +121,6 @@ public class OI {
 	// winch motor
 	public static Button winchMotorUpButton;
 	public static Button winchMotorDownButton;
-	// Ball Arm Buttons
-	public static Button ballArmIn;
-	public static Button ballArmOut;
 	//ball spinner
 	public static Button ballSpinnerOut;
 	public static Button ballSpinnerIn;
@@ -154,14 +137,14 @@ public class OI {
 		arcadeStick = tankLeftStick;
 		// FOLLOW OBJECT
 		// Ultrasonic follow buttons
-		// followButton = new JoystickButton(arcadeStick, 6);
-		// followButton.whileHeld(new ErikFollowObject());
+		followButton = new JoystickButton(arcadeStick, 6);
+		followButton.whileHeld(new ErikFollowObject());
 		// Bottom Lift Buttons
-		bottomLiftUp = new JoystickButton(arcadeStick, 6);
-		bottomLiftDown = new JoystickButton(arcadeStick, 7);
+		bottomLiftUp = new JoystickButton(arcadeStick, ?);
+		bottomLiftDown = new JoystickButton(arcadeStick, ?);
 		//Top Lift Buttons
-		topLiftUp = new JoystickButton(arcadeStick, 8);
-		topLiftDown = new JoystickButton(arcadeStick, 9);
+		topLiftUp = new JoystickButton(arcadeStick, ?);
+		topLiftDown = new JoystickButton(arcadeStick, ?);
 		
 		// WHEEL ARMS
 		// buttons control snowblower motor using CANTalon at 75% power
@@ -170,15 +153,11 @@ public class OI {
 		// WINCH Buttons
 		winchMotorUpButton = new JoystickButton(arcadeStick, 10);
 		winchMotorDownButton = new JoystickButton(arcadeStick, 11);
-		//BALL ARM Buttons
-		ballArmIn = new JoystickButton(arcadeStick, 8);
-		ballArmOut = new JoystickButton(arcadeStick, 9);
-		//Ball Spinner Buttons
-		ballSpinnerIn = new JoystickButton(arcadeStick, 4);
-		ballSpinnerOut = new JoystickButton(arcadeStick, 5);
-			
+		//Ball SPinner Buttons
+		ballSpinnerIn = new JoystickButton(arcadeStick, ?);
+		ballSpinnerOut = new JoystickButton(arcadeStick, ?);
 	}	
-	// WHEEL ARMS STATE
+
 	public static WheelArmState getWheelArmState() {
 		if (armsDownButton.get()) {
 			return WheelArmState.DOWN;
@@ -188,7 +167,7 @@ public class OI {
 			return WheelArmState.STOP;
 		}
 	}
-	// WINCH STATE
+	
 	public static WinchState getWinchState() {
 		if (winchMotorDownButton.get()) {
 			return WinchState.DOWN;
@@ -198,17 +177,7 @@ public class OI {
 			return WinchState.STOP;
 		}
 	}
-	// BALL ARMS STATE
-	public static BallArmsState getBallArmsState() {
-		if (ballArmIn.get()) {
-			return BallArmsState.DOWN;
-		} else if (ballArmOut.get()) {
-			return BallArmsState.UP;
-		} else {
-			return BallArmsState.STOP;
-		}
-	}
-	// BOTTOM LIFT STATE
+	
 	public static BottomLiftState getBottomLiftState(){
 		if (bottomLiftUp.get()) {
 			return BottomLiftState.UP;
@@ -220,7 +189,7 @@ public class OI {
 			return BottomLiftState.STOP;
 		}
 	}
-	// TOP LIFT STATE
+
 	public static TopLiftState getTopLiftState(){
 		if (topLiftUp.get()) {
 			return TopLiftState.UP;
@@ -232,7 +201,7 @@ public class OI {
 			return TopLiftState.STOP;
 		}
 	}
-	// BALL SPINNER STATE
+	
 	public static BallSpinnerState getBallSpinnerState(){
 		if (ballSpinnerOut.get()) {
 			return BallSpinnerState.OUT;
