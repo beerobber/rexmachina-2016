3bb0459af6e9306d987b0e9565d514ecf8218ea2
Chris Taylor <beerobber@gmail.com>
2016-03-07 19:17:47 -0500

    cleanup

--- src/org/usfirst/frc/team5582/rexRobot/commands/RotateWheelArms.java
+++ src/org/usfirst/frc/team5582/rexRobot/commands/RotateWheelArms.java
@@ -25,47 +25,11 @@ public class RotateWheelArms extends CommandBase {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	
-    		/*
-    		OI.WheelArmsState state = OI.getWheelArmsState();
-    		switch (state) {
-    		case DOWN: {
-    			wheelArms.down();
-    			//wheelArms.initializeLimitCounter();
-    			break;
-    		}
-    		case UP: {
-    			wheelArms.up();
-    			/*
-    			if (!wheelArms.isLimitSwitchTripped()) {
-        			wheelArms.up();    				
-    			} else {
-    				wheelArms.stop();
-    			}
-    			
-    			break;
-    		}
-    		case STOP: {
-    			wheelArms.stop();
-    			break;
-    		}
-    		}
-    		*/
     		double power = OI.xboxControllerOne.rightStick.getY();
     		wheelArms.setMotorPower(power);
     		
     		// This puts the position of the wheel arms on the screen
     		SmartDashboard.putNumber("Wheel Arms Position", wheelArms.getWheelArmsPosition());
-    		
-    		/*
-    		double upperLimitValue = 0;
-    		double lowerLimitValue = 0;
-    		if (wheelArms.getWheelArmsPosition() == upperLimitValue) {
-    			wheelArms.setMotorPowerDown(power);
-    		}
-    		if (wheelArms.getWheelArmsPosition() == lowerLimitValue) {
-    			wheelArms.setMotorPowerUp(power);
-    		}
-    		*/
     	}
 
     // Make this return true when this Command no longer needs to run execute()
